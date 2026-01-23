01158b935002f800a949b6ce5a175902af4d011d
Logan Butler <logan.butler@ontariotechu.net>
2026-01-22 19:08:52 -0500

    Adding final comments and cleaning code

--- consumer.py
+++ consumer.py
@@ -8,16 +8,13 @@ files=glob.glob("*.json")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=files[0]
 
 project_id="canvas-griffin-485115-v8" # Needs to be changed when used in different Projects
-topic_name = "smartMeter"
-subscription_id = "smartMeter-sub"
+topic_name = "weather-data"
+subscription_id = "weather-data-sub"
 
 subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
 print(f"Listening for messages on {subscription_path}..\n")
-
-# CSV FORMAT
-# 0: time || 1: profileName || 2: temperature || 3: humidity || 4: pressure
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     # deserialize the message back into JSON then convert it to a Python dictionary
@@ -30,7 +27,7 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     humid = "" if message_data["humidity"] == "" else "Humidity: {}\n".format(message_data["humidity"])
     pressure = "" if message_data["pressure"] == "" else "Pressure: {}\n".format(message_data["pressure"])
 
-    #format final output by combining formatted strings
+    #format final output by combining the formatted strings
     print("{}{}{}{}{}".format(time, location, temp, humid, pressure))
     print("<---------------------------------------------------------------------->")
    
@@ -39,7 +36,8 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
 with subscriber:
     streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
     try:
-        # wait without result() so Keyboard Interrupts are now blocked
+        # wait Keyboard Interrupts are now blocked. 
+        # result() isn't used so Keyboard Interrupts aren't blocked.
         while True:
             sleep(1)
     except KeyboardInterrupt:
--- producer.py
+++ producer.py
@@ -2,23 +2,18 @@ from google.cloud import pubsub_v1
 import glob
 import json
 import csv
-import os 
-import random
-import numpy as np                      
-import time
+import os                      
+from time import sleep
 
 files=glob.glob("*.json")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=files[0]
 
 project_id="canvas-griffin-485115-v8" # Needs to be changed when used in different Projects
-topic_name = "smartMeter"
+topic_name = "weather-data"
 
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_name)
 print(f"Published messages with ordering keys to {topic_path}.")
-
-# CSV FORMAT
-# 0: time || 1: profileName || 2: temperature || 3: humidity || 4: pressure
 
 try:
     with open("Labels.csv", 'r') as file:
@@ -28,26 +23,28 @@ try:
             # Convert an individual row in the CSV into JSON
             message = json.dumps(line)
 
-            # print("")
-            # print(message)
-            # print("")
-
             # Serialize the JSON string
             bMessage = message.encode("utf-8")
 
             try:    
                 
+                # Publish the binary message to the google cloud topic
                 future = publisher.publish(topic_path, bMessage)
                 
+                # wait for the transaction to complete
                 future.result()    
                 print("The messages {} has been published successfully".format(message))
             except: 
                 print("Failed to publish the message")
             
-            time.sleep(.5)
-            break 
+            sleep(.5) # wait is to ensure no messages are missed in sending. As the "future.result()" can still fail.
+
+            # Uncomment the below line to send only one line (for testing)
+            break
 except KeyboardInterrupt:
+    #if the user wants to manually stop the script. Catch the the Interrupt then close the script.
     publisher.stop()
     exit(0)
 
+#if the script completes then stop the publisher
 publisher.stop()
